67d0425caeef3e3ff9ff9c4041c4ceb7b2832761
davidf <davidf@25043fe6-1616-0410-a82d-ba8f324cfe61>
2008-11-14 08:47:23 +0000

    Replace all `StandardError` catches in `DataExchange` with `Exception` catches This is a general cleanup that we did before to the rest of the code Basically errors generated from C libraries or external libraries are usually not `StandardError`s, and we should almost never look for `StandardError`s Also improved import error logging, and removed whitespace
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@7036 25043fe6-1616-0410-a82d-ba8f324cfe61

--- processfamily/ThreadControl.py
+++ processfamily/ThreadControl.py
@@ -23,7 +23,7 @@ def get_thread_callstr(thread):
             thread_name = "%s.%s" % (thread.__class__.__module__, thread.__class__.__name__)
         callargs = ", ".join([repr(arg) for arg in thread_args] + ["%s=%r" % (name, value) for name, value in thread_kwargs.items()])
         return "%s was called with %s(%s)" % (thread.getName(), thread_name, callargs)
-    except StandardError, e:
+    except Exception, e:
         return "Could not calculate thread arguments for thread (error %s)" % e
 
 def graceful_stop_thread(thread, thread_wait=0.5):
@@ -48,7 +48,7 @@ def forceful_stop_thread(thread):
         logging.warning("Stopping thread %s forcefully" % thread.getName())
         try:
             thread._Thread__stop()
-        except StandardError, e:
+        except Exception, e:
             logging.warning("Error stopping thread %s: %s" % (thread.getName(), e))
     return not thread.isAlive()
 
