40080806d2298d082363cc17824a0c983c47f7a7
matth <matth@sjsoft.com>
2014-06-02 13:48:54 +0000

    Moved the body of ThreadMonitor in to j5.OS.
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@19152 25043fe6-1616-0410-a82d-ba8f324cfe61

--- processfamily/ThreadControl.py
+++ processfamily/ThreadControl.py
@@ -4,7 +4,7 @@
 
 NB this is dangerous and should only be used during shutdown; it's possible to get locks etc in unsavoury states through using this code"""
 
-from j5.Control.Admin import ThreadMonitor
+from j5.OS import ThreadMonitor
 
 from j5.Logging import Errors
 import threading
