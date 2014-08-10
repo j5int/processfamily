cd6d2bbd6026a5c620db0e378c4df4ddd9cc5fe5
Matt Hampton <matth@j5int.com>
2014-08-10 21:23:57 +0200

    Rename module.

--- processfamily/test_processfamily.py
+++ processfamily/test_processfamily.py
@@ -6,7 +6,7 @@ from __init__ import ProcessFamily
 
 class TestStartStop(unittest.TestCase):
     def test_start_stop_one(self):
-        family = ProcessFamily(child_process_module_name='processfamily.test.ChildProcessForTests', number_of_child_processes=1)
+        family = ProcessFamily(child_process_module_name='processfamily.test.ChildProcess', number_of_child_processes=1)
         family.start()
         time.sleep(5)
         family.stop()
