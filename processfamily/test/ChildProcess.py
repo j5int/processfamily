1aec67a408048f454dc33e25d14ceb7851f0ca31
Matt Hampton <matth@j5int.com>
2014-08-11 13:23:16 +0200

    Turn my test child process in to a little http server.

--- processfamily/test/ChildProcess.py
+++ processfamily/test/ChildProcess.py
@@ -1,21 +1,45 @@
 __author__ = 'matth'
 
 from processfamily import ChildProcess, start_child_process
-import threading
 import logging
+import BaseHTTPServer
+
+class MyHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """Serve a GET request."""
+        t = self.get_response_text()
+        self.send_head(t)
+        self.wfile.write(t)
+
+    def do_HEAD(self):
+        """Serve a HEAD request."""
+        self.send_head(self.get_response_text())
+
+    def get_response_text(self):
+        return u"Hello World".encode("UTF-8")
+
+    def send_head(self, content):
+        """Common code for GET and HEAD commands.
+
+        This sends the response code and MIME headers.
+
+        """
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.send_header("Content-Length", len(content))
+        self.end_headers()
 
 class ChildProcessForTests(ChildProcess):
 
     def __init__(self):
-        self.stop_event = threading.Event()
+        self.httpd = BaseHTTPServer.HTTPServer(("", 9081), MyHTTPRequestHandler)
 
     def run(self):
-        while not self.stop_event.is_set():
-            logging.info("Child doing stuff")
-            self.stop_event.wait(2)
+        self.httpd.serve_forever()
 
     def stop(self, timeout=None):
-        self.stop_event.set()
+        self.httpd.shutdown()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
