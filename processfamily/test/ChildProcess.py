136af7545f4d58662b0b366eb2f73680770fd7ae
matth <matth@sjsoft.com>
2014-08-14 16:03:48 +0200

    Creating a re-usable 'webserver' object so that the parent and children can open a web port.

--- processfamily/test/ChildProcess.py
+++ processfamily/test/ChildProcess.py
@@ -1,71 +1,19 @@
 __author__ = 'matth'
 
-from processfamily import ChildProcess, start_child_process, ProcessFamily
+from processfamily import ChildProcess, start_child_process
 import logging
-import BaseHTTPServer
-import argparse
-import os
-from processfamily.test import Config
-
-class MyHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        """Serve a GET request."""
-        t = self.get_response_text()
-        self.send_head(t)
-        self.wfile.write(t)
-
-    def do_HEAD(self):
-        """Serve a HEAD request."""
-        self.send_head(self.get_response_text())
-
-    def get_response_text(self):
-        return u"Hello World".encode("UTF-8")
-
-    def send_head(self, content):
-        """Common code for GET and HEAD commands.
-
-        This sends the response code and MIME headers.
-
-        """
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain; charset=utf-8")
-        self.send_header("Content-Length", len(content))
-        self.end_headers()
+from processfamily.test.FunkyWebServer import FunkyWebServer
 
 class ChildProcessForTests(ChildProcess):
 
     def init(self):
-        arg_parser = argparse.ArgumentParser(description='ChildProcessForTests')
-        arg_parser.add_argument('child_number', type=int)
-        arg_parser.add_argument('port', type=int)
-        arg_parser.add_argument('--cpu_number', type=int)
-        args = arg_parser.parse_args()
-        self.child_number = args.child_number
-        self.httpd = BaseHTTPServer.HTTPServer(("", args.port), MyHTTPRequestHandler)
+        self.server = FunkyWebServer()
 
     def run(self):
-        self.httpd.serve_forever()
+        self.server.run()
 
     def stop(self, timeout=None):
-        self.httpd.shutdown()
-
-class ProcessFamilyForTests(ProcessFamily):
-    def __init__(self, number_of_child_processes=None, run_as_script=True):
-        super(ProcessFamilyForTests, self).__init__(
-            child_process_module_name='processfamily.test.ChildProcess',
-            number_of_child_processes=number_of_child_processes,
-            run_as_script=run_as_script)
-
-    def get_child_process_cmd(self, child_number):
-        return super(ProcessFamilyForTests, self).get_child_process_cmd(child_number) + [
-            str(child_number),
-            str(self.get_port_nr(child_number))]
-
-    def get_port_nr(self, child_number):
-        return Config.get_starting_port_nr() + child_number + 1
-
-
+        self.server.stop()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
--- processfamily/test/ParentProcess.py
+++ processfamily/test/ParentProcess.py
@@ -1,15 +1,34 @@
 # -*- coding: utf-8 -*-
 __author__ = 'matth'
 
-from processfamily.test import ChildProcess
+from processfamily import ProcessFamily
+from processfamily.test.FunkyWebServer import FunkyWebServer
 import time
 import logging
+
+class ProcessFamilyForTests(ProcessFamily):
+    def __init__(self, number_of_child_processes=None, run_as_script=True):
+        super(ProcessFamilyForTests, self).__init__(
+            child_process_module_name='processfamily.test.ChildProcess',
+            number_of_child_processes=number_of_child_processes,
+            run_as_script=run_as_script)
+
+    def get_child_process_cmd(self, child_number):
+        return super(ProcessFamilyForTests, self).get_child_process_cmd(child_number) + [
+            '--process_number', str(child_number+1)]
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting")
-    family = ChildProcess.ProcessFamilyForTests(number_of_child_processes=1)
+    server = FunkyWebServer()
+    family = ProcessFamilyForTests(number_of_child_processes=server.num_children)
     family.start()
-    time.sleep(5)
-    family.stop()
+    try:
+        try:
+            server.run()
+        except KeyboardInterrupt:
+            pass
+    finally:
+        family.stop()
+
     logging.info("Done")
