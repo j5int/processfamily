7d4c8feeff51d76c1d304109c3d59d36b640cab2
Matt Hampton <matth@j5int.com>
2014-08-25 14:38:11 +0200

    Find pythonw dynamically.

--- processfamily/test/Config.py
+++ processfamily/test/Config.py
@@ -1,8 +1,9 @@
 __author__ = 'matth'
 
 import os
+import sys
 
-pythonw_exe = "c:\\Python27\\pythonw.exe"
+pythonw_exe = os.path.join(sys.prefix, "pythonw.exe")
 svc_name = 'ProcessFamilyTest'
 def get_starting_port_nr():
     return int(os.environ.get("PROCESSFAMILY_TESTS_STARTING_PORT_NR", "9080"))
