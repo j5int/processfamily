9a75cf836ce881c45ff2f3755ccf714df9b8f895
Yaseen Mowzer <yaseen.mowzer@hexagon.com>
2020-04-07 15:40:40 +0200

    Don't use ProcThreadAttributeHandleListPopen in Python 3
    This commit fixes some unit tests.
    
    On Python 3.7 redirecting the standard handles with close_fds=True
    works on Windows so using ProcThreadAttributeHandleListPopen is unnecessary.
    
    (I'm not sure if it works with earlier versions of Pythonlist

--- processfamily/win32Popen.py
+++ processfamily/win32Popen.py
@@ -11,6 +11,7 @@ from builtins import str
 from past.builtins import basestring
 from builtins import *
 from builtins import object
+from future.utils import PY2
 __author__ = 'matth'
 
 from processfamily import _winprocess_ctypes
@@ -25,6 +26,8 @@ import logging
 import time
 
 logger = logging.getLogger("processfamily.win32Popen")
+
+# TODO Revert file to before futurize and only use these in Python 2
 
 class HandlesOverCommandLinePopen(subprocess.Popen):
     """
@@ -197,165 +200,170 @@ def open_commandline_passed_stdio_streams(args=None):
 
     win32event.SetEvent(event_handle)
 
+if PY2:
+    class ProcThreadAttributeHandleListPopen(subprocess.Popen):
+        """
+        Uses the STARTUPINFOEX struct to pass through an explicit list of
+        handles to inherit. This is used to more closely match the behaviour
+        on linux when the input and output streams are redirected, but you
+        want close_fds to be True. (This is 'not supported' in the standard
+        implementation.)
 
-class ProcThreadAttributeHandleListPopen(subprocess.Popen):
-    """
-    Uses the STARTUPINFOEX struct to pass through an explicit list of
-    handles to inherit. This is used to more closely match the behaviour
-    on linux when the input and output streams are redirected, but you
-    want close_fds to be True. (This is 'not supported' in the standard
-    implementation.)
+        Please note that this functionality requires Windows version > XP/2003.
 
-    Please note that this functionality requires Windows version > XP/2003.
+        Relevant python docs:
+        http://bugs.python.org/issue19764
+        http://legacy.python.org/dev/peps/pep-0446/
+        """
 
-    Relevant python docs:
-    http://bugs.python.org/issue19764
-    http://legacy.python.org/dev/peps/pep-0446/
-    """
+        def __init__(self, args, stdin=None, stdout=None, stderr=None, close_fds=False, shell=False, **kwargs):
+            self.__really_close_fds = close_fds
+            if close_fds and (stdin is not None or stdout is not None or stderr is not None):
+                if not _winprocess_ctypes.CAN_USE_EXTENDED_STARTUPINFO:
+                    raise ValueError("close_fds is not supported on Windows "
+                                     "platforms XP/2003 and below, if you redirect stdin/stdout/stderr")
+                if shell:
+                    raise ValueError("close_fds is not supported on Windows "
+                                     "if you redirect stdin/stdout/stderr, and use shell=True")
+                self.__really_close_fds = True
+                close_fds = False
 
-    def __init__(self, args, stdin=None, stdout=None, stderr=None, close_fds=False, shell=False, **kwargs):
-        self.__really_close_fds = close_fds
-        if close_fds and (stdin is not None or stdout is not None or stderr is not None):
-            if not _winprocess_ctypes.CAN_USE_EXTENDED_STARTUPINFO:
-                raise ValueError("close_fds is not supported on Windows "
-                                 "platforms XP/2003 and below, if you redirect stdin/stdout/stderr")
+            super(ProcThreadAttributeHandleListPopen, self).__init__(
+                args, stdin=stdin, stdout=stdout, stderr=stderr, close_fds=close_fds, shell=shell, **kwargs)
+
+
+        # This Source Code Form is subject to the terms of the Mozilla Public
+        # License, v. 2.0. If a copy of the MPL was not distributed with this file,
+        # You can obtain one at http://mozilla.org/MPL/2.0/.
+        #
+        # This snippet is a modified method taken from : https://hg.mozilla.org/mozilla-central/raw-file/0753f7b93ab7/testing/mozbase/mozprocess/mozprocess/processhandler.py
+        def _execute_child(self, *args_tuple):
+            if sys.hexversion < 0x02070600: # prior to 2.7.6
+                (args, executable, preexec_fn, close_fds,
+                 cwd, env, universal_newlines, input_startupinfo,
+                 creationflags, shell,
+                 p2cread, p2cwrite,
+                 c2pread, c2pwrite,
+                 errread, errwrite) = args_tuple
+                to_close = None
+            elif sys.hexversion > 0x03040000: # 3.4.0 and later
+                (args, executable, preexec_fn, close_fds,
+                                    pass_fds, cwd, env,
+                                    input_startupinfo, creationflags, shell,
+                                    p2cread, p2cwrite,
+                                    c2pread, c2pwrite,
+                                    errread, errwrite,
+                                    restore_signals, start_new_session) = args_tuple
+                to_close = set()
+                to_close.add(p2cread)
+                if p2cwrite is not None:
+                    to_close.add(p2cwrite)
+                to_close.add(c2pwrite)
+                if c2pread is not None:
+                    to_close.add(c2pread)
+                to_close.add(errwrite)
+                if errread is not None:
+                    to_close.add(errread)
+            else: # 2.7.6 and later
+                (args, executable, preexec_fn, close_fds,
+                 cwd, env, universal_newlines, input_startupinfo,
+                 creationflags, shell, to_close,
+                 p2cread, p2cwrite,
+                 c2pread, c2pwrite,
+                 errread, errwrite) = args_tuple
+
             if shell:
-                raise ValueError("close_fds is not supported on Windows "
-                                 "if you redirect stdin/stdout/stderr, and use shell=True")
-            self.__really_close_fds = True
-            close_fds = False
+                return super(ProcThreadAttributeHandleListPopen, self)._execute_child(*args_tuple)
 
-        super(ProcThreadAttributeHandleListPopen, self).__init__(
-            args, stdin=stdin, stdout=stdout, stderr=stderr, close_fds=close_fds, shell=shell, **kwargs)
+            close_fds = self.__really_close_fds
 
+            if not isinstance(args, basestring):
+                args = subprocess.list2cmdline(args)
 
-    # This Source Code Form is subject to the terms of the Mozilla Public
-    # License, v. 2.0. If a copy of the MPL was not distributed with this file,
-    # You can obtain one at http://mozilla.org/MPL/2.0/.
-    #
-    # This snippet is a modified method taken from : https://hg.mozilla.org/mozilla-central/raw-file/0753f7b93ab7/testing/mozbase/mozprocess/mozprocess/processhandler.py
-    def _execute_child(self, *args_tuple):
-        if sys.hexversion < 0x02070600: # prior to 2.7.6
-            (args, executable, preexec_fn, close_fds,
-             cwd, env, universal_newlines, input_startupinfo,
-             creationflags, shell,
-             p2cread, p2cwrite,
-             c2pread, c2pwrite,
-             errread, errwrite) = args_tuple
-            to_close = None
-        elif sys.hexversion > 0x03040000: # 3.4.0 and later
-            (args, executable, preexec_fn, close_fds,
-                                pass_fds, cwd, env,
-                                input_startupinfo, creationflags, shell,
-                                p2cread, p2cwrite,
-                                c2pread, c2pwrite,
-                                errread, errwrite,
-                                restore_signals, start_new_session) = args_tuple
-            to_close = set()
-            to_close.add(p2cread)
-            if p2cwrite is not None:
-                to_close.add(p2cwrite)
-            to_close.add(c2pwrite)
-            if c2pread is not None:
-                to_close.add(c2pread)
-            to_close.add(errwrite)
-            if errread is not None:
-                to_close.add(errread)
-        else: # 2.7.6 and later
-            (args, executable, preexec_fn, close_fds,
-             cwd, env, universal_newlines, input_startupinfo,
-             creationflags, shell, to_close,
-             p2cread, p2cwrite,
-             c2pread, c2pwrite,
-             errread, errwrite) = args_tuple
+            if _winprocess_ctypes.CAN_USE_EXTENDED_STARTUPINFO:
+                attribute_list_data = ()
+                startupinfoex           = _winprocess_ctypes.STARTUPINFOEX()
+                startupinfo             = startupinfoex.StartupInfo
+                startupinfo.cb          = _winprocess_ctypes.sizeof(_winprocess_ctypes.STARTUPINFOEX)
+                startupinfo_argument = startupinfoex
+            else:
+                startupinfo = _winprocess_ctypes.STARTUPINFO()
+                startupinfo_argument = startupinfo
 
-        if shell:
-            return super(ProcThreadAttributeHandleListPopen, self)._execute_child(*args_tuple)
+            if input_startupinfo is not None:
+                startupinfo.dwFlags = input_startupinfo.dwFlags
+                startupinfo.hStdInput = input_startupinfo.hStdInput
+                startupinfo.hStdOutput = input_startupinfo.hStdOutput
+                startupinfo.hStdError = input_startupinfo.hStdError
+                startupinfo.wShowWindow = input_startupinfo.wShowWindow
 
-        close_fds = self.__really_close_fds
+            inherit_handles = 0 if close_fds else 1
 
-        if not isinstance(args, basestring):
-            args = subprocess.list2cmdline(args)
+            if None not in (p2cread, c2pwrite, errwrite):
+                if close_fds:
+                    HandleArray = _winprocess_ctypes.HANDLE * 3
+                    # PY2COMPAT new_int doesn't seem to know how to convert file handles to int.
+                    # So we use the "native_int" instead.
+                    handles_to_inherit = HandleArray(native_int(p2cread), native_int(c2pwrite), native_int(errwrite))
 
-        if _winprocess_ctypes.CAN_USE_EXTENDED_STARTUPINFO:
-            attribute_list_data = ()
-            startupinfoex           = _winprocess_ctypes.STARTUPINFOEX()
-            startupinfo             = startupinfoex.StartupInfo
-            startupinfo.cb          = _winprocess_ctypes.sizeof(_winprocess_ctypes.STARTUPINFOEX)
-            startupinfo_argument = startupinfoex
-        else:
-            startupinfo = _winprocess_ctypes.STARTUPINFO()
-            startupinfo_argument = startupinfo
+                    attribute_list_data = (
+                        (
+                            _winprocess_ctypes.PROC_THREAD_ATTRIBUTE_HANDLE_LIST,
+                            handles_to_inherit
+                        ),
+                    )
+                    inherit_handles = 1
 
-        if input_startupinfo is not None:
-            startupinfo.dwFlags = input_startupinfo.dwFlags
-            startupinfo.hStdInput = input_startupinfo.hStdInput
-            startupinfo.hStdOutput = input_startupinfo.hStdOutput
-            startupinfo.hStdError = input_startupinfo.hStdError
-            startupinfo.wShowWindow = input_startupinfo.wShowWindow
-
-        inherit_handles = 0 if close_fds else 1
-
-        if None not in (p2cread, c2pwrite, errwrite):
-            if close_fds:
-                HandleArray = _winprocess_ctypes.HANDLE * 3
+                startupinfo.dwFlags |= _winprocess_ctypes.STARTF_USESTDHANDLES
                 # PY2COMPAT new_int doesn't seem to know how to convert file handles to int.
                 # So we use the "native_int" instead.
-                handles_to_inherit = HandleArray(native_int(p2cread), native_int(c2pwrite), native_int(errwrite))
+                startupinfo.hStdInput = native_int(p2cread)
+                startupinfo.hStdOutput = native_int(c2pwrite)
+                startupinfo.hStdError = native_int(errwrite)
 
-                attribute_list_data = (
-                    (
-                        _winprocess_ctypes.PROC_THREAD_ATTRIBUTE_HANDLE_LIST,
-                        handles_to_inherit
-                    ),
-                )
-                inherit_handles = 1
+            if _winprocess_ctypes.CAN_USE_EXTENDED_STARTUPINFO:
+                attribute_list = _winprocess_ctypes.ProcThreadAttributeList(attribute_list_data)
+                startupinfoex.lpAttributeList = attribute_list.value
+                creationflags |= _winprocess_ctypes.EXTENDED_STARTUPINFO_PRESENT
 
-            startupinfo.dwFlags |= _winprocess_ctypes.STARTF_USESTDHANDLES
-            # PY2COMPAT new_int doesn't seem to know how to convert file handles to int.
-            # So we use the "native_int" instead.
-            startupinfo.hStdInput = native_int(p2cread)
-            startupinfo.hStdOutput = native_int(c2pwrite)
-            startupinfo.hStdError = native_int(errwrite)
+            def _close_in_parent(fd):
+                fd.Close()
+                if to_close:
+                    to_close.remove(fd)
 
-        if _winprocess_ctypes.CAN_USE_EXTENDED_STARTUPINFO:
-            attribute_list = _winprocess_ctypes.ProcThreadAttributeList(attribute_list_data)
-            startupinfoex.lpAttributeList = attribute_list.value
-            creationflags |= _winprocess_ctypes.EXTENDED_STARTUPINFO_PRESENT
+            # create the process
+            try:
+                hp, ht, pid, tid = _winprocess_ctypes.ExtendedCreateProcess(
+                    executable, args,
+                    None, None, # No special security
+                    inherit_handles, #Inherit handles
+                    creationflags,
+                    _winprocess_ctypes.EnvironmentBlock(env) if env else None,
+                    cwd,
+                    startupinfo_argument)
+            finally:
+                # Child is launched. Close the parent's copy of those pipe
+                # handles that only the child should have open.  You need
+                # to make sure that no handles to the write end of the
+                # output pipe are maintained in this process or else the
+                # pipe will not close when the child process exits and the
+                # ReadFile will hang.
+                if p2cread is not None:
+                    _close_in_parent(p2cread)
+                if c2pwrite is not None:
+                    _close_in_parent(c2pwrite)
+                if errwrite is not None:
+                    _close_in_parent(errwrite)
 
-        def _close_in_parent(fd):
-            fd.Close()
-            if to_close:
-                to_close.remove(fd)
+            self._child_created = True
+            self._handle = subprocess.Handle(hp) if hasattr(subprocess, 'Handle') else hp
+            self._thread = ht
+            self.pid = pid
+            self.tid = tid
 
-        # create the process
-        try:
-            hp, ht, pid, tid = _winprocess_ctypes.ExtendedCreateProcess(
-                executable, args,
-                None, None, # No special security
-                inherit_handles, #Inherit handles
-                creationflags,
-                _winprocess_ctypes.EnvironmentBlock(env) if env else None,
-                cwd,
-                startupinfo_argument)
-        finally:
-            # Child is launched. Close the parent's copy of those pipe
-            # handles that only the child should have open.  You need
-            # to make sure that no handles to the write end of the
-            # output pipe are maintained in this process or else the
-            # pipe will not close when the child process exits and the
-            # ReadFile will hang.
-            if p2cread is not None:
-                _close_in_parent(p2cread)
-            if c2pwrite is not None:
-                _close_in_parent(c2pwrite)
-            if errwrite is not None:
-                _close_in_parent(errwrite)
-
-        self._child_created = True
-        self._handle = subprocess.Handle(hp) if hasattr(subprocess, 'Handle') else hp
-        self._thread = ht
-        self.pid = pid
-        self.tid = tid
-
-        ht.Close()
+            ht.Close()
+else:
+    # On Python 3.7 closefds with redirection works on windows, so we don't need to change anything.
+    # I'm not sure if this is true for early versions of Python 3
+    class ProcThreadAttributeHandleListPopen(subprocess.Popen):
+        pass
